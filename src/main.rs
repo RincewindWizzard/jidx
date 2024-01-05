b55c96851c243a57d17b73c8a870b30910f333e7
RincewindWizzard <git@magierdinge.de>
2024-01-05 11:57:39 +0100

    tests writing

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,14 @@ use crate::args::Args;
 use crate::json_path::{JsonPath, Value};
 use crate::json_path::ToJsonPath;
 use crate::json_path::Value::EmptyArray;
-use crate::token::StackElement;
+use crate::token::{JsonIndexable, StackElement};
 use crate::token::StackElement::ArrayIndex;
 
 mod json_path;
 mod token;
 mod args;
 
-const EXAMPLE_JSON: &str = "./mars_weather.json";
+const EXAMPLE_JSON: &str = "./testdata/mars_weather.json";
 const BUFFER_SIZE: usize = 1024;
 
 
@@ -31,130 +31,6 @@ fn main() {
     read_json().unwrap();
 }
 
-
-struct JsonIndexIterator {
-    token_stack: Vec<StackElement>,
-    stream: Stream<File>,
-}
-
-pub trait JsonIndexable<'a> {
-    fn index_paths(self) -> JsonIndexIterator;
-}
-
-impl<'a> JsonIndexable<'a> for Stream<File> {
-    fn index_paths(self) -> JsonIndexIterator {
-        JsonIndexIterator {
-            token_stack: vec![],
-            stream: self,
-        }
-    }
-}
-
-
-impl JsonIndexIterator {
-    fn clean_stack(&mut self) {
-        if let Some(StackElement::EndObject) = self.token_stack.last() {
-            loop {
-                let popped = self.token_stack.pop();
-                debug!("popped: {:?}", popped);
-                debug!("Stack: {:?}", self.token_stack);
-                match popped {
-                    None => { break; }
-                    Some(x) => {
-                        match x {
-                            StackElement::StartObject => {
-                                break;
-                            }
-                            _ => {}
-                        }
-                    }
-                }
-            }
-        }
-
-        loop {
-            let popped = self.token_stack.pop();
-            debug!("popped: {:?}", popped);
-            debug!("Stack: {:?}", self.token_stack);
-            match popped {
-                None => { break; }
-                Some(x) => {
-                    match x {
-                        StackElement::JsKey(_) => {
-                            break;
-                        }
-                        _ => {}
-                    }
-                }
-            }
-        }
-    }
-
-    fn inc_array_index(&mut self) -> usize {
-        if let Some(StackElement::ArrayIndex(n)) = self.token_stack.last() {
-            let last_index = self.token_stack.len() - 1;
-            let n = n + 1;
-            self.token_stack[last_index] = StackElement::ArrayIndex(n);
-            return n;
-        }
-        0
-    }
-    fn next(&mut self) -> anyhow::Result<Option<(JsonPath, Value)>> {
-        loop {
-            match self.stream.next()? {
-                None => {
-                    return Ok(None);
-                }
-                Some(token) => {
-                    let token = StackElement::from(token);
-                    debug!("Got token: {:?}", token);
-                    self.token_stack.push(token.clone());
-                    debug!("Stack: {:?}", self.token_stack);
-                    let path = self.token_stack.as_json_path();
-
-
-                    // push array index onto the stack
-                    if let StackElement::StartArray = token {
-                        self.token_stack.push(ArrayIndex(0));
-                    }
-
-                    // return empty array as single value
-                    if let StackElement::EndArray = token {
-                        let last_index = self.token_stack.len();
-                        let array_len = match &self.token_stack[last_index - 2] {
-                            ArrayIndex(i) => { *i }
-                            _ => { 0 }
-                        };
-                        debug!("Array len: {array_len}");
-                        self.clean_stack();
-                        if array_len == 0 {
-                            return Ok(Some((path, EmptyArray)));
-                        }
-                    }
-
-
-                    if let Some(value) = token.as_value() {
-                        debug!("Found value: {value}");
-                        // remove token from stack
-                        self.token_stack.pop();
-                        let index = self.inc_array_index();
-                        if index == 0 {
-                            self.clean_stack();
-                        }
-                        return Ok(Some((path, value)));
-                    }
-
-
-                    if let StackElement::EndObject = token {
-                        self.clean_stack();
-                    }
-                }
-            }
-        }
-        Ok(None)
-    }
-}
-
 fn read_json() -> anyhow::Result<()> {
     let file = File::open(EXAMPLE_JSON)?;
     let mut stream = Stream::from_read(file)?.index_paths();
@@ -165,7 +41,7 @@ fn read_json() -> anyhow::Result<()> {
                 break;
             }
             Some((key, value)) => {
-                println!("Token: {key} -> {value}");
+                println!("{key} -> {value}");
             }
         }
     }
--- src/token.rs
+++ src/token.rs
@@ -1,7 +1,11 @@
+use std::fs::File;
+use log::debug;
 use qjsonrs::JsonToken;
+use qjsonrs::sync::{Stream, TokenIterator};
 
-use crate::json_path::Value;
-use crate::json_path::Value::{Boolean, Null, Number};
+use crate::json_path::{JsonPath, ToJsonPath, Value};
+use crate::json_path::Value::{Boolean, EmptyArray, Null, Number};
+use crate::token::StackElement::ArrayIndex;
 
 /// A token from a stream of JSON.
 #[derive(Debug, PartialEq, Clone)]
@@ -59,6 +63,174 @@ impl From<JsonToken<'_>> for StackElement {
             JsonToken::JsNumber(n) => { StackElement::JsNumber(n.to_string()) }
             JsonToken::JsString(s) => { StackElement::JsString(s.into_raw_str().to_string()) }
             JsonToken::JsKey(key) => { StackElement::JsKey(key.into_raw_str().to_string()) }
+        }
+    }
+}
+
+pub struct JsonIndexIterator {
+    token_stack: Vec<StackElement>,
+    stream: Stream<File>,
+}
+
+pub trait JsonIndexable {
+    fn index_paths(self) -> JsonIndexIterator;
+}
+
+impl JsonIndexable for Stream<File> {
+    fn index_paths(self) -> JsonIndexIterator {
+        JsonIndexIterator {
+            token_stack: vec![],
+            stream: self,
+        }
+    }
+}
+
+
+impl JsonIndexIterator {
+    fn clean_stack(&mut self) {
+        if let Some(StackElement::EndObject) = self.token_stack.last() {
+            loop {
+                let popped = self.token_stack.pop();
+                debug!("popped: {:?}", popped);
+                debug!("Stack: {:?}", self.token_stack);
+                match popped {
+                    None => { break; }
+                    Some(x) => {
+                        match x {
+                            StackElement::StartObject => {
+                                break;
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+            }
+        }
+
+        loop {
+            let popped = self.token_stack.pop();
+            debug!("popped: {:?}", popped);
+            debug!("Stack: {:?}", self.token_stack);
+            match popped {
+                None => { break; }
+                Some(x) => {
+                    match x {
+                        StackElement::JsKey(_) => {
+                            break;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+        }
+    }
+
+    fn inc_array_index(&mut self) -> usize {
+        if let Some(StackElement::ArrayIndex(n)) = self.token_stack.last() {
+            let last_index = self.token_stack.len() - 1;
+            let n = n + 1;
+            self.token_stack[last_index] = StackElement::ArrayIndex(n);
+            return n;
+        }
+        0
+    }
+    pub(crate) fn next(&mut self) -> anyhow::Result<Option<(JsonPath, Value)>> {
+        loop {
+            match self.stream.next()? {
+                None => {
+                    return Ok(None);
+                }
+                Some(token) => {
+                    let token = StackElement::from(token);
+                    debug!("Got token: {:?}", token);
+                    self.token_stack.push(token.clone());
+                    debug!("Stack: {:?}", self.token_stack);
+
+
+                    // push array index onto the stack
+                    if let StackElement::StartArray = token {
+                        self.token_stack.push(ArrayIndex(0));
+                    }
+
+                    // return empty array as single value
+                    if let StackElement::EndArray = token {
+                        let last_index = self.token_stack.len();
+                        let array_len = match &self.token_stack[last_index - 2] {
+                            ArrayIndex(i) => { *i }
+                            _ => { 0 }
+                        };
+                        debug!("Array len: {array_len}");
+                        let path = self.token_stack.as_json_path();
+                        self.clean_stack();
+                        if array_len == 0 {
+                            return Ok(Some((path, EmptyArray)));
+                        }
+                    }
+
+
+                    if let Some(value) = token.as_value() {
+                        debug!("Found value: {value}");
+                        let path = self.token_stack.as_json_path();
+                        // remove token from stack
+                        self.token_stack.pop();
+                        let index = self.inc_array_index();
+                        if index == 0 {
+                            self.clean_stack();
+                        }
+                        return Ok(Some((path, value)));
+                    }
+
+
+                    if let StackElement::EndObject = token {
+                        self.clean_stack();
+                    }
+                }
+            }
+        }
+        Ok(None)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use std::fs::File;
+    use qjsonrs::sync::Stream;
+    use crate::token::JsonIndexable;
+
+    const EXAMPLE_JSON: &str = "./testdata/mars_weather.json";
+
+    #[test]
+    fn test_parse_doc() {
+        let file = File::open(crate::EXAMPLE_JSON).unwrap();
+        let mut stream = Stream::from_read(file).unwrap().index_paths();
+
+        let mut result = HashMap::new();
+        loop {
+            match stream.next().unwrap() {
+                None => {
+                    break;
+                }
+                Some((key, value)) => {
+                    result.insert(key, value);
+                }
+            }
+        }
+
+        assert_eq!(result.len(), 35);
+
+        let parsed: serde_json::Value = serde_json::from_str(include_str!("../testdata/mars_weather_flattened.json"))
+            .expect("Failed to parse JSON");
+
+        // Konvertieren des serde_json::Value in eine HashMap
+        if let serde_json::Value::Object(map) = parsed {
+            let mut hashmap: HashMap<String, serde_json::Value> = HashMap::new();
+            for (key, value) in map {
+                hashmap.insert(key, value);
+            }
+
+            // Jetzt hast du die JSON-Daten in einer HashMap
+            println!("{:?}", hashmap);
         }
     }
 }
